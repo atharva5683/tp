341edff66c19199706dffd2d73a7aca2c5bef525
Atharva Kumbhar <160429511+atharva5683@users.noreply.github.com>
2025-07-23 17:06:39 +0530

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "app_server" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
-  user_data              = file("${path.module}/user_data.sh.tpl")
+  user_data              = file("${path.module}/../scripts/user_data.sh.tpl")
   
   # Force new resource creation
   lifecycle {
